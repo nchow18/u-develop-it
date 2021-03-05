d1f428b9f35ef6474ffaf423b33e180eb89b3517
Nathan Chow <emailme@nathanchow.ca>
2021-03-05 14:40:57 -0500

    update candidates

--- server.js
+++ server.js
@@ -2,16 +2,40 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const sqlite3 = require('sqlite3').verbose();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+      return console.error(err.message);
+    }
+  
+    console.log('Connected to the election database.');
+  });
+
+// Start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
+
+db.all(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 // Default response for any other requests(Not Found) Catch all
 app.use((req, res) => {
   res.status(404).end();
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+
+
+
+
+
